fba3e8df05fed27f41a21cc0a44b50065311e552
Sneaky Koder <sneaky.koder@gmail.com>
2016-10-10 20:54:30 -0700

    Config file, change tab by spaces

--- site_monitor_config.py
+++ site_monitor_config.py
@@ -1,30 +1,30 @@
 #!/usr/bin/python3
 
 config = {
-	'sites': [
-		{	# Reference Site, to review if there a internet access
-			'reference': True,
-			'name': 'Google',
-			'host': 'google.com', 
-			'url': 'https://www.google.com', 
-			'timeout': 10
-		},
-		{	# Template Site
-			'enabled': True,
-			'name': 'Google',
-			'host': 'google.com', 
-			'url': 'https://www.google.com', 
-			'timeout': 10,
-			'notify': ['']
-		}
-	],
-	'smtp': {
-		'server': '',
-		'port': 0,
-		'login': {'user': '', 'password': ''},
-		'from_address': ''
-	},
-	'global': {
-		'notify': []
-	}
+    'sites': [
+        {    # Reference Site, to review if there a internet access
+            'reference': True,
+            'name': 'Google',
+            'host': 'google.com',
+            'url': 'https://www.google.com',
+            'timeout': 10
+        },
+        {    # Template Site
+            'enabled': True,
+            'name': 'Google',
+            'host': 'google.com',
+            'url': 'https://www.google.com',
+            'timeout': 10,
+            'notify': ['']
+        }
+    ],
+    'smtp': {
+        'server': '',
+        'port': 0,
+        'login': {'user': '', 'password': ''},
+        'from_address': ''
+    },
+    'global': {
+        'notify': []
+    }
 }
